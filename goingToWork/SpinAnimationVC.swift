85f0eaf3ba6540f47ce9c4a7c91ba159038636cd
Jeremiah <brennin001@knights.gannon.edu>
2019-11-08 13:59:30 -0500

    Add "Spin !" button to GameStoryboard along with "SpinAnimationVC.swift"

---
+++ goingToWork/SpinAnimationVC.swift
@@ -0,0 +1,29 @@
+//
+//  SpinAnimationVC.swift
+//  goingToWork
+//
+//  Created by Jeremiah Brennin on 11/7/19.
+//  Copyright © 2019 Gannon University. All rights reserved.
+//
+
+import UIKit
+
+class SpinAnimationVC: UIViewController {
+    
+    @IBOutlet weak var Spinner: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func SpinButton(_ sender: Any) {
+        UIView.animate(withDuration: 4.0, animations: ({
+            self.Spinner.transform = CGAffineTransform(rotationAngle: 360)
+        }))
+    }
+}
+

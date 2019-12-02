1f4858d9972f789385a42189bcf93b8b56afbded
Mary Kutschke <kutschke001@knights.gannon.edu>
2019-12-02 13:24:33 -0500

    Fix image picker controller issue

--- goingToWork/Vehicle.swift
+++ goingToWork/Vehicle.swift
@@ -12,37 +12,34 @@ class Vehicle: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
-    //    var images = [String]?
-    //
-    //    override func viewDidLoad() {
-    //        images = ["image1.png","image2.png"]
-    //        let img : UIImage = UIImage(named: images[index])!
-    //        imageButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
-    //        button.frame = CGRectMake(100, 100, 100, 100)//Set your own frame
-    //        imageButton.setImage(img,forState:UIControlState.Normal)
-    //    }
-//
-//    override func viewDidLoad() {
-//        var UITapRecognizer = UITapGestureRecognizer(target: self, action: "La_Boutique_Dei_Colori_Car02")
-//        self.imageView.addGestureRecognizer(UITapRecognizer)
-//        self.imageView.isUserInteractionEnabled = true
-//    }
-//
-//    @IBAction func pickCar(sender: AnyObject) {
-//        addNewCar()
-//    }
-//
-//    func addNewCar() {
-//        let picker = UIImagePickerController()
-//        picker.allowsEditing = true
-//        picker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
-//        //presentViewController(picker, animated: true, completion: nil)
-//    }
-//
-//    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-//        //postingImage.image = image
-//        self.dismiss(animated: true, completion: nil)
-//    }
+    var images: [String] = []
+    
+    override func viewDidLoad() {
+        images = ["La_Boutique_Dei_Colori_Car02.png","La_Boutique_Dei_Colori_Car03.png","La_Boutique_Dei_Colori_Car01","La_Boutique_Dei_Colori_Car04"]
+            var UITapRecognizer = UITapGestureRecognizer(target: self, action: "La_Boutique_Dei_Colori_Car02.png")
+            self.imageView.addGestureRecognizer(UITapRecognizer)
+            self.imageView.isUserInteractionEnabled = true
+          //let img : UIImage = UIImage(named: images[index])!
+         //imageButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        //button.frame = CGRectMake(100, 100, 100, 100)//Set your own frame
+         //imageButton.setImage(img,forState:UIControlState.Normal)
+    }
+
+    @IBAction func pickCar(sender: AnyObject) {
+        addNewCar()
+    }
+
+    func addNewCar() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        //Vehicle(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        //postingImage.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func playGameButton(_ sender: Any) {
         performSegue(withIdentifier: "toGameBoard", sender: nil)

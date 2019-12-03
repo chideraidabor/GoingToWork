0054b01f9ef86786506ced5a904fbf0f84e0917d
Kurt Do <do006@knights.gannon.edu>
2019-12-03 00:18:04 -0500

    Set up char model selection options

--- goingToWork/PlayerCharDetailVC.swift
+++ goingToWork/PlayerCharDetailVC.swift
@@ -8,24 +8,151 @@
 
 import UIKit
 
-class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    var imageArray = [UIImage(named: "male_generic1"),UIImage(named: "male_generic2"),UIImage(named: "male_generic3")]
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as? ImageCollectionViewCell
-        cell?.ImgImage.image=imageArray[indexPath.row]
-        return cell!
-    }
-    
+class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
+    var chartype: Int = 1;
 
+    var playerSelectorVC: PlayerPickerController?
+
+    @IBOutlet weak var male1Selected: UIButton!
+    
+    @IBOutlet weak var male2Selected: UIButton!
+    
+    @IBOutlet weak var male3Selected: UIButton!
+    
+    @IBOutlet weak var female1Selected: UIButton!
+    
+    @IBOutlet weak var female2Selected: UIButton!
+    
+    @IBOutlet weak var female3Selected: UIButton!
+    
+    @IBOutlet weak var Confirm: UIButton!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        male1Selected.isSelected=true
+        male1Selected.isUserInteractionEnabled = true
+         male2Selected.isUserInteractionEnabled = true
+         male3Selected.isUserInteractionEnabled = true
+         female1Selected.isUserInteractionEnabled = true
+         female2Selected.isUserInteractionEnabled = true
+         female3Selected.isUserInteractionEnabled = true
+        
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func selectMale1(_ sender: UIButton) {
+        //sender.isHighlighted=true
+              if !(sender.isSelected){
+                sender.isSelected=true
+                sender.backgroundColor=UIColor.red
+                chartype = 1;
+
+            }
+            else {
+                sender.isSelected=false
+                sender.backgroundColor=UIColor.white
+            }
+
+        
+    }
+    @IBAction func selectMale2(_ sender: UIButton) {
+        //sender.isHighlighted=true
+               if !(sender.isSelected){
+                sender.isSelected=true
+                sender.backgroundColor=UIColor.red
+                chartype = 2;
+
+            }
+            else {
+                sender.isSelected=false
+                sender.backgroundColor=UIColor.white
+            }
+
+        
+    }
+    @IBAction func selectMale3(_ sender: UIButton) {
+        //sender.isHighlighted=true
+        if !(sender.isSelected){
+            sender.isSelected=true
+            sender.backgroundColor=UIColor.red
+            chartype = 3;
+
+        }
+        else {
+            sender.isSelected=false
+            sender.backgroundColor=UIColor.white
+        }
+
+    }
+    @IBAction func selectFemale1(_ sender: UIButton) {
+        //sender.isHighlighted=true
+             if !(sender.isSelected){
+                sender.isSelected=true
+                sender.backgroundColor=UIColor.red
+                chartype = 4;
+
+            }
+            else {
+                sender.isSelected=false
+                sender.backgroundColor=UIColor.white
+            }
+
+        
+
+    }
+    @IBAction func selectFemale2(_ sender: UIButton) {
+        //sender.isHighlighted=true
+                if !(sender.isSelected){
+                sender.isSelected=true
+                sender.backgroundColor=UIColor.red
+                    chartype = 5;
+
+            }
+            else {
+                sender.isSelected=false
+                sender.backgroundColor=UIColor.white
+            }
+
+        
+
+    }
+    @IBAction func selectFemale3(_ sender: UIButton) {
+        //sender.isHighlighted=true
+                if !(sender.isSelected){
+                sender.isSelected=true
+                sender.backgroundColor=UIColor.red
+                    chartype = 6;
+
+            }
+            else {
+                sender.isSelected=false
+                sender.backgroundColor=UIColor.white
+            }
+
+        
+    }
+    func tapGesture(){
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //for setting sth on vehicle (passing data)
+        //if segue.destination is Vehicle{
+        
+            //let vc = segue.destination as? Vehicle
+            /*/
+            {
+                do sth (set a
+            }
+         */
+        }
+    //ref: learnappmaking.com (pass data vc)
+    //todo: Pass data between VC (playerpicker?)
+    //possible ways:
+    //1. reference to vehicle and playerpicker (above)
+    //2. protocol/delegation
+    //3. closure
+    
 }

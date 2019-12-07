eaa07b64640c16dd5d172b138ea8759e6ecacd8b
Kurt Do <mcslender97@gmail.com>
2019-12-07 18:04:22 -0500

    Setup loop for PlayerDetail to modify each player and display current player Make button color when deselected looks consistent

--- goingToWork/PlayerCharDetailVC.swift
+++ goingToWork/PlayerCharDetailVC.swift
@@ -11,7 +11,9 @@ import UIKit
 class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     var chartype: Int = 0;
     var  noOfPlayer = 0
+    
     var playerSelectorVC: PlayerPickerController?
+  //  var job
     
 
     @IBOutlet weak var char1: UIButton!
@@ -29,42 +31,43 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBOutlet weak var Confirm: UIButton!
 
     @IBOutlet weak var charSelected: UILabel!
+    
+    @IBOutlet weak var PlayerNumber: UILabel!
+    
+    
     override func viewDidLoad(){
-        super.viewDidLoad()
-       
-        
-        char1.isUserInteractionEnabled = true
-         char2.isUserInteractionEnabled = true
-         char3.isUserInteractionEnabled = true
-         char4.isUserInteractionEnabled = true
-         char5.isUserInteractionEnabled = true
-         char6.isUserInteractionEnabled = true
-        
-        
+        for i in 1...noOfPlayer {
+            super.viewDidLoad()
+            PlayerNumber.text = "Player" + String(i) + ", please select your character"
+            char1.isUserInteractionEnabled = true
+             char2.isUserInteractionEnabled = true
+             char3.isUserInteractionEnabled = true
+             char4.isUserInteractionEnabled = true
+             char5.isUserInteractionEnabled = true
+             char6.isUserInteractionEnabled = true
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    fileprivate func selectCharButton(_ sender: UIButton) {
+    fileprivate func selectOption(_ sender: UIButton) {
         sender.isSelected=true
         sender.backgroundColor=UIColor.red
     }
-    fileprivate func deselectCharButton(_ sender: UIButton) {
+    fileprivate func deselectOption(_ sender: UIButton) {
         sender.isSelected=false
         sender.backgroundColor=UIColor.white
     }
 
-
-
     @IBAction func selectChar1(_ sender: UIButton) {
         //sender.isHighlighted=true
         if !(sender.isSelected){
-             selectCharButton(sender)
-            deselectCharButton(char2)
-            deselectCharButton(char3)
-            deselectCharButton(char4)
-            deselectCharButton(char5)
-            deselectCharButton(char6)
+             selectOption(sender)
+            deselectOption(char2)
+            deselectOption(char3)
+            deselectOption(char4)
+            deselectOption(char5)
+            deselectOption(char6)
             }
         else {
           
@@ -76,15 +79,14 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBAction func selectChar2(_ sender: UIButton) {
         //sender.isHighlighted=true
         if !(sender.isSelected){
-            selectCharButton(sender)
-            deselectCharButton(char1)
-                      deselectCharButton(char3)
-                      deselectCharButton(char4)
-                      deselectCharButton(char5)
-                      deselectCharButton(char6)
+            selectOption(sender)
+            deselectOption(char1)
+            deselectOption(char3)
+            deselectOption(char4)
+            deselectOption(char5)
+            deselectOption(char6)
             }
         else {
-    
             }
 
         chartype = 2;
@@ -95,12 +97,12 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBAction func selectChar3(_ sender: UIButton) {
         //sender.isHighlighted=true
         if !(sender.isSelected){
-            selectCharButton(sender)
-            deselectCharButton(char1)
-                      deselectCharButton(char2)
-                      deselectCharButton(char4)
-                      deselectCharButton(char5)
-                      deselectCharButton(char6)
+            selectOption(sender)
+            deselectOption(char1)
+                      deselectOption(char2)
+                      deselectOption(char4)
+                      deselectOption(char5)
+                      deselectOption(char6)
         }
         else {
             
@@ -112,12 +114,12 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBAction func selectChar4(_ sender: UIButton) {
         //sender.isHighlighted=true
              if !(sender.isSelected){
-              selectCharButton(sender)
-                deselectCharButton(char1)
-                          deselectCharButton(char2)
-                          deselectCharButton(char3)
-                          deselectCharButton(char5)
-                          deselectCharButton(char6)
+              selectOption(sender)
+                deselectOption(char1)
+                          deselectOption(char2)
+                          deselectOption(char3)
+                          deselectOption(char5)
+                          deselectOption(char6)
             }
             else {
                
@@ -130,12 +132,12 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBAction func selectChar5(_ sender: UIButton) {
         //sender.isHighlighted=true
         if !(sender.isSelected){
-             selectCharButton(sender)
-            deselectCharButton(char1)
-                      deselectCharButton(char2)
-                      deselectCharButton(char3)
-                      deselectCharButton(char4)
-                      deselectCharButton(char6)
+             selectOption(sender)
+            deselectOption(char1)
+                      deselectOption(char2)
+                      deselectOption(char3)
+                      deselectOption(char4)
+                      deselectOption(char6)
             }
         else {
                 
@@ -148,12 +150,12 @@ class PlayerCharDetailVC: UIViewController, UICollectionViewDelegate {
     @IBAction func selectChar6(_ sender: UIButton) {
         //sender.isHighlighted=true
         if !(sender.isSelected){
-           selectCharButton(sender)
-            deselectCharButton(char2)
-                      deselectCharButton(char3)
-                      deselectCharButton(char4)
-                      deselectCharButton(char5)
-                      deselectCharButton(char1)
+           selectOption(sender)
+            deselectOption(char2)
+                      deselectOption(char3)
+                      deselectOption(char4)
+                      deselectOption(char5)
+                      deselectOption(char1)
             }
         else {
                 

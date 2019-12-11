0ecd38ecfabe0829d08db4623e92fd88d12f973b
chidera idabor <idabor001@gannon.edu>
2019-12-10 20:01:37 -0500

    uipickerview displays numbers but now it does not read the Game core data

--- goingToWork/PlayerPickerController.swift
+++ goingToWork/PlayerPickerController.swift
@@ -8,18 +8,15 @@
 
 import Foundation
 import UIKit
-//import CoreData
+import CoreData
 
-class PlayerPickerController: UIViewController{
-
-   
-    // NSFetchedResultsControllerDelegate
- //  @IBOutlet weak var label: UILabel!
+class PlayerPickerController: UIViewController, NSFetchedResultsControllerDelegate {
+var managedObjectContext: NSManagedObjectContext? = nil
     @IBOutlet weak var PlayerPicker: UIPickerView!
     private let dataSource = ["2", "3","4"]
-     var myGame = Game()
+    var myGame = Game()
+  
     private var count: Int64 = 0
-   
 
 
     override func viewDidLoad() {
@@ -45,10 +42,11 @@ extension PlayerPickerController: UIPickerViewDelegate, UIPickerViewDataSource{
         return dataSource.count
     }
 
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        //
+    private func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+
         count = (Int64(dataSource[row]))!
         myGame.numberOfPlayers = (Int64(dataSource[row]))!
+        
     }
     
     internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -59,6 +57,7 @@ extension PlayerPickerController: UIPickerViewDelegate, UIPickerViewDataSource{
         if segue.identifier == "charDetail" {
                 let controller = segue.destination as! PlayerCharDetail
             controller.noOfPlayer =  Int(myGame.numberOfPlayers)
+            //performSegue(withIdentifier: "charDetail", sender: self)
             }
         }
 
@@ -66,42 +65,5 @@ extension PlayerPickerController: UIPickerViewDelegate, UIPickerViewDataSource{
     
     
     }
-//class PlayerPickerController: UIViewController, UIPickerViewDelegate{
-//
-//    // 2. Create an array of data to display in the picker view
-//    var pickerArrayItems = ["Cat", "Dog", "Hamster", "Goldfish", "Parrot", "Lizard"]
-//
-//    // 3. Create an IBOutlet for the picker view on the user interface
-//    @IBOutlet weak var pickerViewUI: UIPickerView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // 4. Set the delegate of the picker view control to the view controller
-//        pickerViewUI.delegate = self
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//    // 5. Define the number of parts or components of the picker view
-//    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    // 6. Define the number of items to appear in the picker view
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return pickerArrayItems.count
-//    }
-//
-//    // 7. Define the title of each row of the picker view with the array items (Step 2 above)
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return pickerArrayItems[row]
-//    }
-//
-//}
- 
-    
 
 
---
+++ goingToWork/Storyboards/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  goingToWork
+//
+//  Created by Idabor, Chidera L on 12/10/19.
+//  Copyright © 2019 Gannon University. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class gameData: NSManagedObject {
+
+    
+    @NSManaged var noOfPlayers: Int
+// Insert code here to add functionality to your managed object subclass
+
+}
+//var currentCourse = Course.init(entity: NSEntityDescription.entityForName("Course", inManagedObjectContext:mox)!, insertIntoManagedObjectContext: mox)

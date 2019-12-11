930f24d69a4819de783f2438ee98ea2f7c4c71c3
Aram <alsihli001@knights.gannon.edu>
2019-12-11 16:34:21 -0500

    popUpVC

---
+++ goingToWork/popUpVC.swift
@@ -0,0 +1,21 @@
+//
+//  popUpVC.swift
+//  goingToWork
+//
+//  Created by Aram Alsihli on 12/11/19.
+//  Copyright © 2019 Gannon University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class popUpVC : UIViewController{
+    @IBAction func CloseWindow(_ sender: Any) {
+    self.dismiss(animated: true, completion: nil);
+        
+
+    }
+    
+    
+    
+}

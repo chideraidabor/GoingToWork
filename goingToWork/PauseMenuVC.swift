6cdd6b38c3199bbcb026156e8f1502f9c2c8c809
Nelson Castillo <castillo003@knights.gannon.edu>
2019-12-11 12:07:03 -0500

    Pausemenu Storyboard
    Created PauseMenu button, segues to VC

---
+++ goingToWork/PauseMenuVC.swift
@@ -0,0 +1,20 @@
+//
+//  PauseMenuVC.swift
+//  goingToWork
+//
+//  Created by Nelson Castillo on 12/11/19.
+//  Copyright © 2019 Gannon University. All rights reserved.
+//
+
+import UIKit
+
+class PauseMenuVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
